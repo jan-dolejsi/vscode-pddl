0f5ce2b5d456abc9023780aec073ec07da348ded
Jan Dolejsi <JDolejsi@slb.com>
2020-04-07 14:28:49 +0200

    diag

--- client/src/test/suite/ValDownload.test.ts
+++ client/src/test/suite/ValDownload.test.ts
@@ -51,6 +51,16 @@ async function mkdirIfDoesNotExist(path: fs.PathLike, options: fs.MakeDirectoryO
     });
 }
 
+async function getFiles(dir: string): Promise<string[]> {
+    const dirents = await afs.readdir(dir, { withFileTypes: true });
+    const childrenFilePromises = dirents.map((dirent) => {
+        const res = path.resolve(dir, dirent.name);
+        return dirent.isDirectory() ? getFiles(res) : Promise.resolve([res]);
+    });
+    const files = await Promise.all(childrenFilePromises);
+    return Array.prototype.concat(...files);
+}
+  
 suite('VAL Download and Configuration', () => {
     let origValStepPath: string | undefined;
     let origValConfiguration: string | undefined;
@@ -91,72 +101,78 @@ suite('VAL Download and Configuration', () => {
         const extensionContext = await createTestExtensionContext();
         const pddlConf = vscode.workspace.getConfiguration(CONF_PDDL);
 
-        // GIVEN
-        const valFolderPath = path.join(extensionContext.globalStoragePath, 'val');
-        await mkdirIfDoesNotExist(valFolderPath, { mode: 0o777, recursive: true });
-        const valManifestPath = path.join(valFolderPath, 'VAL.version');
-        const previousValManifest = {
-            "buildId": 24,
-            "version": "20190805.2",
-            "files": [
-              "Val-20190805.2-win64/bin/ValStep.exe",
-              "Val-20190805.2-win64/bin/PinguPlan.exe",
-              "Val-20190805.2-win64/bin/DomainView.exe",
-              "Val-20190805.2-win64/bin/Analyse.exe",
-              "Val-20190805.2-win64/bin/TIM.exe",
-              "Val-20190805.2-win64/bin/libwinpthread-1.dll",
-              "Val-20190805.2-win64/bin/libVAL.dll",
-              "Val-20190805.2-win64/bin/PlanRec.exe",
-              "Val-20190805.2-win64/bin/Relax.exe",
-              "Val-20190805.2-win64/bin/Validate.exe",
-              "Val-20190805.2-win64/bin/ToFn.exe",
-              "Val-20190805.2-win64/bin/HowWhatWhen.exe",
-              "Val-20190805.2-win64/bin/PlanSeqStep.exe",
-              "Val-20190805.2-win64/bin/libgcc_s_seh-1.dll",
-              "Val-20190805.2-win64/bin/PlanToValStep.exe",
-              "Val-20190805.2-win64/bin/Instantiate.exe",
-              "Val-20190805.2-win64/bin/Parser.exe",
-              "Val-20190805.2-win64/bin/ValueSeq.exe",
-              "Val-20190805.2-win64/bin/libstdc++-6.dll",
-              "Val-20190805.2-win64/bin/TypeAnalysis.exe",
-              "Val-20190805.2-win64/README.md"
-            ],
-            // "parserPath": "Val-20190805.2-win64/bin/Parser.exe",
-            // "validatePath": "Val-20190805.2-win64/bin/Validate.exe",
-            // "valueSeqPath": "Val-20190805.2-win64/bin/ValueSeq.exe",
-            // "valStepPath": "Val-20190805.2-win64/bin/ValStep.exe"
-        };
-        console.log('>>> BEFORE writeFile(valManifestPath, JSON.stringify(previousValManifest, null, 2))');
-        await utils.afs.writeFile(valManifestPath, JSON.stringify(previousValManifest, null, 2));
-        console.log('>>> AFTER writeFile(valManifestPath, JSON.stringify(previousValManifest, null, 2))');
+        try {
+            // GIVEN
+            const valFolderPath = path.join(extensionContext.globalStoragePath, 'val');
+            await mkdirIfDoesNotExist(valFolderPath, { mode: 0o777, recursive: true });
+            const valManifestPath = path.join(valFolderPath, 'VAL.version');
+            const previousValManifest = {
+                "buildId": 24,
+                "version": "20190805.2",
+                "files": [
+                    "Val-20190805.2-win64/bin/ValStep.exe",
+                    "Val-20190805.2-win64/bin/PinguPlan.exe",
+                    "Val-20190805.2-win64/bin/DomainView.exe",
+                    "Val-20190805.2-win64/bin/Analyse.exe",
+                    "Val-20190805.2-win64/bin/TIM.exe",
+                    "Val-20190805.2-win64/bin/libwinpthread-1.dll",
+                    "Val-20190805.2-win64/bin/libVAL.dll",
+                    "Val-20190805.2-win64/bin/PlanRec.exe",
+                    "Val-20190805.2-win64/bin/Relax.exe",
+                    "Val-20190805.2-win64/bin/Validate.exe",
+                    "Val-20190805.2-win64/bin/ToFn.exe",
+                    "Val-20190805.2-win64/bin/HowWhatWhen.exe",
+                    "Val-20190805.2-win64/bin/PlanSeqStep.exe",
+                    "Val-20190805.2-win64/bin/libgcc_s_seh-1.dll",
+                    "Val-20190805.2-win64/bin/PlanToValStep.exe",
+                    "Val-20190805.2-win64/bin/Instantiate.exe",
+                    "Val-20190805.2-win64/bin/Parser.exe",
+                    "Val-20190805.2-win64/bin/ValueSeq.exe",
+                    "Val-20190805.2-win64/bin/libstdc++-6.dll",
+                    "Val-20190805.2-win64/bin/TypeAnalysis.exe",
+                    "Val-20190805.2-win64/README.md"
+                ],
+                // "parserPath": "Val-20190805.2-win64/bin/Parser.exe",
+                // "validatePath": "Val-20190805.2-win64/bin/Validate.exe",
+                // "valueSeqPath": "Val-20190805.2-win64/bin/ValueSeq.exe",
+                // "valStepPath": "Val-20190805.2-win64/bin/ValStep.exe"
+            };
+            console.log('>>> BEFORE writeFile(valManifestPath, JSON.stringify(previousValManifest, null, 2))');
+            await utils.afs.writeFile(valManifestPath, JSON.stringify(previousValManifest, null, 2));
+            console.log('>>> AFTER writeFile(valManifestPath, JSON.stringify(previousValManifest, null, 2))');
 
-        const myValStepPath = 'MyValStep.exe';
-        await pddlConf.update(VAL_STEP_PATH, myValStepPath, vscode.ConfigurationTarget.Global);
-        await pddlConf.update(VALIDATION_PATH, path.join('val', 'Val-20190805.2-win64', 'bin', 'Validate.exe'), vscode.ConfigurationTarget.Global);
-        await pddlConf.update(VALUE_SEQ_PATH, path.join('val', 'Val-20190805.2-win64', 'bin', 'ValueSeq.exe'), vscode.ConfigurationTarget.Global);
-        await vscode.workspace.getConfiguration(PDDL_PARSER)
-            .update(EXECUTABLE_OR_SERVICE, path.join('val', 'Val-20190805.2-win64', 'bin', 'Parser.exe'), vscode.ConfigurationTarget.Global);
+            const myValStepPath = 'MyValStep.exe';
+            await pddlConf.update(VAL_STEP_PATH, myValStepPath, vscode.ConfigurationTarget.Global);
+            await pddlConf.update(VALIDATION_PATH, path.join('val', 'Val-20190805.2-win64', 'bin', 'Validate.exe'), vscode.ConfigurationTarget.Global);
+            await pddlConf.update(VALUE_SEQ_PATH, path.join('val', 'Val-20190805.2-win64', 'bin', 'ValueSeq.exe'), vscode.ConfigurationTarget.Global);
+            await vscode.workspace.getConfiguration(PDDL_PARSER)
+                .update(EXECUTABLE_OR_SERVICE, path.join('val', 'Val-20190805.2-win64', 'bin', 'Parser.exe'), vscode.ConfigurationTarget.Global);
 
-        const askedToOverwrite: string[] = [];
+            const askedToOverwrite: string[] = [];
 
-        const shouldOverwrite = function (toolName: string, yourConfiguredPath: string, newToolPath: string): boolean {
-            console.log(`toolName: ${toolName}, yourConfiguredPath: ${yourConfiguredPath}, newToolPath: ${newToolPath}`);
-            askedToOverwrite.push(toolName);
-            return toolName !== 'ValStep';
-        };
+            const shouldOverwrite = function (toolName: string, yourConfiguredPath: string, newToolPath: string): boolean {
+                console.log(`toolName: ${toolName}, yourConfiguredPath: ${yourConfiguredPath}, newToolPath: ${newToolPath}`);
+                askedToOverwrite.push(toolName);
+                return toolName !== 'ValStep';
+            };
     
-        // WHEN
-        const valDownloader = new TestableValDownloader(extensionContext, shouldOverwrite);
-        await valDownloader.downloadConfigureAndCleanUp();
+            // WHEN
+            const valDownloader = new TestableValDownloader(extensionContext, shouldOverwrite);
+            await valDownloader.downloadConfigureAndCleanUp();
 
-        // THEN
-        const conf = new PddlConfiguration(extensionContext);
-        const actualValStepPath = await conf.getValStepPath();
-        expect(actualValStepPath).to.equal(myValStepPath);
-        const actualValueSeqPath = conf.getValueSeqPath();
-        expect(actualValStepPath).to.not.be.undefined;
-        assert.startsWith(actualValueSeqPath!, path.join(valFolderPath, 'Val-20190911.1-win64'), "value seq path");
-        expect(askedToOverwrite).to.deep.equal(['ValStep']);
+            // THEN
+            const conf = new PddlConfiguration(extensionContext);
+            const actualValStepPath = await conf.getValStepPath();
+            expect(actualValStepPath).to.equal(myValStepPath);
+            const actualValueSeqPath = conf.getValueSeqPath();
+            expect(actualValStepPath).to.not.be.undefined;
+            assert.startsWith(actualValueSeqPath!, path.join(valFolderPath, 'Val-20190911.1-win64'), "value seq path");
+            expect(askedToOverwrite).to.deep.equal(['ValStep']);
+        }
+        finally {
+            const allStoredFiles = await getFiles(extensionContext.globalStoragePath);
+            console.log(`All files in the mocked global storage: \n` + allStoredFiles.join('\n'));
+        }
     }).timeout(5000);
 
     after(async () => {
