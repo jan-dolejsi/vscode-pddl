ee944a4f4ad88386a665659d07b4f32957684ad2
Jan Dolejsi <JDolejsi@slb.com>
2021-03-14 23:19:18 +0100

    added npm package

--- switch-to-local.cmd
+++ switch-to-local.cmd
@@ -2,6 +2,8 @@
 
 call npm uninstall pddl-workspace
 call npm uninstall ai-planning-val
+call npm uninstall pddl-planning-service-client
 
 call npm install ..\pddl-workspace --save
 call npm install ..\ai-planning-val.js --save
+call npm install ..\pddl-planning-service-client --save
