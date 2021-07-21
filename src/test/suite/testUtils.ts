83b207e444e05209551723c7536271a187340a7b
Jan Dolejsi <JDolejsi@slb.com>
2021-07-21 10:03:07 +0200

    freezing npm package versions

--- src/test/suite/testUtils.ts
+++ src/test/suite/testUtils.ts
@@ -41,9 +41,10 @@ class MockMemento implements Memento {
     constructor() {
         this.map = new Map<string, unknown>();
     }
-    get keys(): string[] {
-        return [...this.map.keys()];
-    }
+    // will be needed for a future version of VS Code?
+    // get keys(): string[] {
+    //     return [...this.map.keys()];
+    // }
     get<T>(key: string, defaultValue?: T): T {
         return (this.map.get(key) as T) ?? assertDefined(defaultValue, "Default value not specified");
     }
