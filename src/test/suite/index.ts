9d08879412e8a3906303830a8cfbae9645a64ca7
Jan Dolejsi <JDolejsi@slb.com>
2023-10-26 00:56:04 +0200

    glob upgrade

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import { globSync } from 'glob';
 
 export function run(): Promise<void> {
 	// Create the mocha test
@@ -9,30 +9,26 @@ export function run(): Promise<void> {
 		color: true,
 	});
 	mocha.timeout(10000);
-	
+
 	const testsRoot = path.resolve(__dirname, '..');
 
 	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
+		const files = globSync('***/**.test.js', { cwd: testsRoot });
 
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+		// Add files to the test suite
+		files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
-					}
-				});
-			} catch (err) {
-				e(err);
-			}
-		});
+		try {
+			// Run the mocha test
+			mocha.run(failures => {
+				if (failures > 0) {
+					e(new Error(`${failures} tests failed.`));
+				} else {
+					c();
+				}
+			});
+		} catch (err) {
+			e(err);
+		}
 	});
 }
